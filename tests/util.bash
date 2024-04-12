070a137a0d0c847e61632c80bc9a39de32f08f0c
Ivan <fizmat.r66@gmail.com>
2024-04-12 18:30:01 +0400

    refactor(tests): echo inside functions

--- tests/util.bash
+++ tests/util.bash
@@ -2,11 +2,13 @@
 FAILURES=0
 
 apply() {
+    echo Apply "$@"
     sudo puppet apply --detailed-exitcodes --debug "$@" || [ $? -eq 2 ]
     FAILURES=$((FAILURES + $?))
 }
 
 check() {
+    echo Check "$@"
     "$@"
     FAILURES=$((FAILURES + $?))
 }
