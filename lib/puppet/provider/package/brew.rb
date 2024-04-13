dc88c26867ca2d0f825fbc822002fed3401ec03e
Ivan <fizmat.r66@gmail.com>
2024-04-14 00:16:51 +0400

    lint(provider): prefer keyword arguments for boolean arguments with a boolean default
    they are actually keyword arguments in the parent method anyway

--- lib/puppet/provider/package/brew.rb
+++ lib/puppet/provider/package/brew.rb
@@ -24,7 +24,7 @@ Puppet::Type.type(:package).provide(:brew, parent: Puppet::Provider::Package) do
   commands brew: @brewbin
   commands stat: '/usr/bin/stat'
 
-  def self.execute(cmd, failonfail = false, combine = false)
+  def self.execute(cmd, failonfail: false, combine: false)
     owner = stat('-nf', '%Uu', @brewbin).to_i
     group = stat('-nf', '%Ug', @brewbin).to_i
     home  = Etc.getpwuid(owner).dir
